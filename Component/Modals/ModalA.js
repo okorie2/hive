1a062b1f00e5257dd268e3f2954562ea016674b5
Okorie Emmanuella <ellahokorie@gmail.com>
2022-01-16 07:38:08 +0100

    created a hook for the modal

--- Component/Modals/ModalA.js
+++ Component/Modals/ModalA.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./modala.module.css";
+import UseOutsideAlerter from "../../pages/chat/utilities/UseOutsideAlerter";
 export default function ModalA({ onClick, expanded, ref }) {
+  const modalRef = useRef(null);
+  UseOutsideAlerter(modalRef);
   return (
     <>
       <div
         className={styles.modalA}
         id={expanded ? styles["modalAx"] : ""}
-        ref={ref}
+        ref={modalRef}
       >
         <div onClick={onClick}>Contact Info</div>
         <div>Name Contact </div>
