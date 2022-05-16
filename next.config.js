5ef685b2c502ca66f62b7ae2d00e9726504f930d
Okorie Emmanuella <ellahokorie@gmail.com>
2022-05-16 23:38:07 +0100

    updated next config to support sitemap

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['img.icons8.com'],
+    domains: ["img.icons8.com"],
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
   },
 };
